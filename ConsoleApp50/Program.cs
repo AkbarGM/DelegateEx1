398347f2d77fcd8822d9879b53c443b76da650c9
AkbarGM <56382572+AkbarGM@users.noreply.github.com>
2020-04-13 13:27:59 +0500

    Add project files.

---
+++ ConsoleApp50/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp50
+{
+    class Program
+    {
+        delegate int Operation(int a, int b);
+        static void Main(string[] args)
+        {
+            Operation operation;
+            operation = Add;
+            Console.WriteLine(operation(3, 4));
+            operation = Multiply;
+            Console.WriteLine(operation(3, 4));
+
+            Console.ReadLine();
+
+        }
+        static int Add(int a, int b)
+        {
+            return a + b;
+        }
+        static int Multiply(int a, int b)
+        {
+            return a * b;
+        }
+    }
+}
